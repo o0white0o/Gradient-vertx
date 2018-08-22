f9e2b4b136afa0fea21db95a008b9bb8d6f04a14
o0white0o <melvin260596@gmail.com>
2018-08-22 03:58:28 +0300

    Fixed bug with compilation

--- simple-method/src/main/java/com/o0white0o/simplemethod/SingleFlow.java
+++ simple-method/src/main/java/com/o0white0o/simplemethod/SingleFlow.java
@@ -1,4 +1,28 @@
 package com.o0white0o.simplemethod;
 
-public class SingleFlow {
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import scala.Tuple2;
+import com.o0white0o.functions.LoadData;
+
+
+import java.util.List;
+
+public class SingleFlow extends AbstractVerticle {
+    private Tuple2<List<List<Double>>,List<Double>> setOfPoints;
+
+
+    @Override
+    public void start(Future<Void> future) throws Exception {
+        setOfPoints = new LoadData("../set_of_points.data").getSetOfPoints();
+        List<List<Double>> vectorRegressors = setOfPoints._1();
+        List<Double> vectorValues = setOfPoints._2();
+        long startTime = System.currentTimeMillis();
+        for (Double value:vectorValues) {
+
+        }
+
+    }
+
 }
